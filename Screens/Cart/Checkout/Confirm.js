6b95133ca80227ceb91aabf468e7de9fb87f3309
rommel dalisay <rommelmdalisay@gmail.com>
2025-01-25 08:23:37 +0800

    confirm order

--- Screens/Cart/Checkout/Confirm.js
+++ Screens/Cart/Checkout/Confirm.js
@@ -1,9 +1,105 @@
-import React from 'react'
-import { View,Text } from 'react-native'
-const Confirm = () => {
-  return (
-     <View><Text>Confirm</Text></View>
-  )
-}
+import React, { useState } from 'react'
+import { View, StyleSheet, Dimensions, ScrollView, Button, Text } from "react-native";
+import { Surface, Avatar, Divider } from 'react-native-paper';
 
-export default Confirm
+
+import { useNavigation } from '@react-navigation/native';
+import { useSelector, useDispatch } from 'react-redux'
+// import AsyncStorage from "@react-native-async-storage/async-storage"
+
+
+var { width, height } = Dimensions.get("window");
+
+const Confirm = (props) => {
+    const [token, setToken] = useState();
+    // const confirm = props.route.params;
+    const finalOrder = props.route.params;
+    console.log("order", finalOrder)
+    const dispatch = useDispatch()
+    // let navigation = useNavigation()
+
+    
+    return (
+        <Surface>
+            <ScrollView contentContainerStyle={styles.container}>
+                <View style={styles.titleContainer}>
+                    <Text style={{ fontSize: 20, fontWeight: "bold" }}>Confirm Order</Text>
+                    {props.route.params ? (
+                        <View style={{ borderWidth: 1, borderColor: "orange" }}>
+                            <Text style={styles.title}>Shipping to:</Text>
+                            <View style={{ padding: 8 }}>
+                                <Text>Address: {finalOrder.order.order.shippingAddress1}</Text>
+                                <Text>Address2: {finalOrder.order.order.shippingAddress2}</Text>
+                                <Text>City: {finalOrder.order.order.city}</Text>
+                                <Text>Zip Code: {finalOrder.order.order.zip}</Text>
+                                <Text>Country: {finalOrder.order.order.country}</Text>
+                            </View>
+                            <Text style={styles.title}>items</Text>
+
+                            {finalOrder.order.order.orderItems.map((item) => {
+                                return (
+                                  <Surface pl="4" pr="5" py="2" bg="white" keyExtractor={item => item.id}>
+
+                                  <Avatar.Image size={48} source={{
+                                      uri: item.image ?
+                                          item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                                  }} />
+                  
+                                  <Text>
+                                      {item.name}
+                                  </Text>
+                  
+                                  <Divider />
+                                  <Text fontSize="xs" color="coolGray.800" alignSelf="flex-start">
+                                      $ {item.price}
+                                  </Text>
+                  
+                  
+                              </Surface>
+                                )
+                            })}
+                        </View>
+                    ) : null}
+                    <View style={{ alignItems: "center", margin: 20 }}>
+                        <Button
+                            title={"Place order"}
+                            // onPress={confirmOrder}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
+        </Surface>
+    )
+
+}
+const styles = StyleSheet.create({
+    container: {
+        height: height,
+        padding: 8,
+        alignContent: "center",
+        backgroundColor: "white",
+    },
+    titleContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 8,
+    },
+    title: {
+        alignSelf: "center",
+        margin: 8,
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    listItem: {
+        alignItems: "center",
+        backgroundColor: "white",
+        justifyContent: "center",
+        width: width / 1.2,
+    },
+    body: {
+        margin: 10,
+        alignItems: "center",
+        flexDirection: "row",
+    },
+});
+export default Confirm;
--- Screens/Cart/Checkout/Payment.js
+++ Screens/Cart/Checkout/Payment.js
@@ -5,101 +5,81 @@ import { Surface, RadioButton, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
 const methods = [
-    { name: 'Cash on Delivery', value: 1 },
-    { name: 'Bank Transfer', value: 2 },
-    { name: 'Card Payment', value: 3 }
+  { name: 'Cash on Delivery', value: 1 },
+  { name: 'Bank Transfer', value: 2 },
+  { name: 'Card Payment', value: 3 }
 ]
 
 const paymentCards = [
-    { name: 'Wallet', value: 1 },
-    { name: 'Visa', value: 2 },
-    { name: 'MasterCard', value: 3 },
-    { name: 'Other', value: 4 }
+  { name: 'Wallet', value: 1 },
+  { name: 'Visa', value: 2 },
+  { name: 'MasterCard', value: 3 },
+  { name: 'Other', value: 4 }
 ]
 
 const Payment = ({ route }) => {
 
-    const order = route.params;
-    const [selected, setSelected] = useState('');
-    const [card, setCard] = useState('');
-    console.log(order)
-    const navigation = useNavigation()
-    return (
-        <View style={styles.container}  >
-             <Text variant="displayLarge">Choose your payment method</Text>
-            <Surface  width="100%"  >
-                <RadioButton.Group
-                    name="myRadioGroup"
-                    value={selected}
-                    onValueChange={(value) => {
-                        setSelected(value);
-                    }}
+  const order = route.params;
+  const [selected, setSelected] = useState('');
+  const [status, setStatus] = useState('unchecked');
+  const [card, setCard] = useState('');
+  console.log(order)
+  const navigation = useNavigation()
+  return (
+    <View style={styles.container}  >
+      <Text variant="displaySmall">Choose your payment method</Text>
+      <Surface width="100%"  >
+        <RadioButton.Group
+          name="myRadioGroup"
+          value={selected}
+          onValueChange={(value) => {
+            setSelected(value)
+            // setStatus('checked')
 
-                >
-                    {console.log(selected)}
-                    {methods.map((item, index) => {
-                        return (
-                            <>
-                             <RadioButton
-                                key={index}
-                                value={item.value} my="1"
-                                colorScheme="green"
-                                size="22"
-                                style={{ float: 'right' }}
-                               
+          }}
 
-                            >
-                                
-                            </RadioButton>
-                            <Text>{item.name}</Text>
-                            </>
-                           
-                        )
-                    })
-                    }
-                </RadioButton.Group>
-            </Surface>
-            {/* {selected === 3 ? (
-                <Box>
-                    <Select
-                        minWidth="100%"
-                        placeholder="Choose Service"
-                        selectedValue={card}
-                        onValueChange={(x) => setCard(x)}
-                        _selectedItem={{
-                            bg: "teal.600",
-                            endIcon: <CheckIcon size="5" />
-                        }}
-                    >
-                        {console.log(card)}
-                        {paymentCards.map((c, index) => {
-                            return (
-                                <Select.Item
-                                    key={c.name}
-                                    label={c.name}
-                                    value={c.name} />
-                            )
-                        })}
 
-                    </Select>
-                </Box>
-            ) : null} */}
-            <View style={{ marginTop: 60, alignSelf: 'center' }}>
-                <Button
-                    title={"Confirm"}
-                    onPress={() => navigation.navigate("Confirm", { order })} />
-            </View>
-        </View>
-    )
+        >
+
+          {methods.map((item, index) => {
+            return (
+
+              <RadioButton.Item
+                key={index}
+                value={item.value}
+
+                color='red'
+                size="18"
+                // style={{ float: 'left' }}
+                // position='trailing'
+                label={item.name}
+                status='checked'
+
+              >
+
+              </RadioButton.Item>
+            )
+          })
+          }
+        </RadioButton.Group>
+      </Surface>
+      
+      <View style={{ marginTop: 60, alignSelf: 'center' }}>
+        <Button
+          title={"Confirm"}
+          onPress={() => navigation.navigate("Confirm", { order })} />
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-       
-    },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
 })
 export default Payment;
