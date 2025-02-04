8134d8ffe31bc9691a00c41a3f942ec31abad396
rommel dalisay <rommelmdalisay@gmail.com>
2025-02-04 13:46:54 +0800

    category

--- Navigators/AdminNavigator.js
+++ Navigators/AdminNavigator.js
@@ -3,8 +3,8 @@ import { createStackNavigator } from "@react-navigation/stack"
 
 // import Orders from "../Screens/Admin/Orders"
 import Products from "../Screens/Admin/Products"
-// import ProductForm from "../Screens/Admin/ProductForm"
-// import Categories from "../Screens/Admin/Categories"
+import ProductForm from "../Screens/Admin/ProductForm"
+import Categories from "../Screens/Admin/Categories"
 
 const Stack = createStackNavigator();
 
@@ -19,9 +19,9 @@ const AdminNavigator= () => {
                     title: "Products"
                 }}
             />
-            {/* <Stack.Screen name="Categories" component={Categories} />
-            <Stack.Screen name="Orders" component={Orders} />
-            <Stack.Screen name="ProductForm" component={ProductForm} /> */}
+            <Stack.Screen name="Categories" component={Categories} />
+            {/* <Stack.Screen name="Orders" component={Orders} /> */}
+             <Stack.Screen name="ProductForm" component={ProductForm} />
         </Stack.Navigator>
     )
 }
--- Screens/Product/ProductContainer.js
+++ Screens/Product/ProductContainer.js
@@ -168,7 +168,7 @@ const ProductContainer = () => {
                                 return (
                                     <ProductList
                                         // navigation={props.navigation}
-                                        key={item._id.$oid}
+                                        key={item.id}
                                         item={item}
                                     />
                                 )
